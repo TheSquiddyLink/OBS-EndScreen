9d18952d681730ae5d0fa871f40860a9bd7fc580
TheSquiddyLink <preston@happychaps.com>
2024-08-13 18:13:16 -0700

    Move Animation to JS

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 const image = document.getElementById("image");
 const text = document.getElementById("text");
-
+const container = document.getElementById("container");
 /**
  * @typedef {Object} Option
  * @property {string} text
@@ -12,14 +12,36 @@ const text = document.getElementById("text");
  */
 var config
 
+const aniLenMs = 10000;
+const aniLenS = aniLenMs / 1000;
+
+var index = 0
+
+var len;
 
 async function main(){
+    container.style.animation = `slide ${aniLenS}s infinite`;
     const raw = await fetch("config/config.json");
     const rawStr = await raw.text();
     config = await JSON.parse(rawStr);
+    len = config.length;
 
-    image.src = "assets/" + config[0].image + ".png";
-    text.innerHTML = config[0].text;
+    update();
+    while(true){
+        await sleep(aniLenMs - 100);
+        console.log("done");
+        index++;
+        if(index >= len) index = 0;
+        update();
+    }
+
 }
 
+function update(){
+    image.src = "assets/" + config[index].image + ".png";
+    text.innerHTML = config[index].text;
+}
 main();
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
