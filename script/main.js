d4b6b5c24959c2617122dfacc0434116147aa0b2
TheSquiddyLink <preston@happychaps.com>
2024-08-13 17:56:51 -0700

    Read From JSON

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const image = document.getElementById("image");
+const text = document.getElementById("text");
+
+/**
+ * @typedef {Object} Option
+ * @property {string} text
+ * @property {string} image
+ */
+
+/**
+ * @type {Array<Option>}
+ */
+var config
+
+
+async function main(){
+    const raw = await fetch("config/config.json");
+    const rawStr = await raw.text();
+    config = await JSON.parse(rawStr);
+
+    image.src = "assets/" + config[0].image + ".png";
+    text.innerHTML = config[0].text;
+}
+
+main();
